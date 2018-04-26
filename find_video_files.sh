95a6348408388ef0be8303023de3d59f3f46f7d0
Cleptes <js3445@student.uni-lj.si>
2018-04-26 13:49:55 -0400

    dodal

---
+++ find_video_files.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+find . -type f -name "*.mkv" -o -name "*.flv" -o -name "*.webm" -o -name "*.vob" -o -name "*.ogg" -o -name "*.ogv" -o -name "*.drc" -o -name "*.avi" -o -name "*.wmv" -o -name "*.mov" -o -name "*.mp4" -o -name "*.mp2" -o -name "*.flv"
